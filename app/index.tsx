293a8d4727e3de891c2fecdfff20fe1ed3cd526c
Eleazar Udo <elzoremmanuel@gmail.com>
2025-01-18 12:44:27 +0100

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text>Another Piece of Text</Text>
+      </View>
+      <View>
+        <Text>Hello World!!! 👋</Text>
+      </View>
+      <Button title="My first Button" />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
